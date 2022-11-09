2c8218f8023857205fcaadde277e3164f53dc85d
IchwanSetyawan <ichwansetyawan2015@gmail.com>
2022-11-09 14:08:46 +0700

    pure function

--- 02-javascript-basic/soal-03/index.js
+++ 02-javascript-basic/soal-03/index.js
@@ -1,16 +1,24 @@
 // SOAL NOMOR 3
-function randomNumber(value){
-  var number = value;
-  var x = 0;
-  while(x !== number ||x <= number){
-      if(x>=number){
-          return console.log('data sudah mencapai '+number)
-      }
+// function randomNumber(value){
+//   var number = value;
+//   var x = 0;
+//   while(x !== number ||x <= number){
+//       if(x>=number){
+//           return console.log('data sudah mencapai '+number)
+//       }
+//     x = Math.random();
+//     console.log(x);
+//   }
+// }
+// const args = [0.9]
+// args.forEach((item)=>{
+//   return randomNumber(item)
+// })
+function randomNumber(x) {
+  while(x <= 0.9){
     x = Math.random();
     console.log(x);
   }
 }
-const args = [0.9]
-args.forEach((item)=>{
-  return randomNumber(item)
-})
+
+randomNumber(0)
